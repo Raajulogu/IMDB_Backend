cd4aa4191a38d2d60acd41b69424a853e09f285b
Raajulogu <rajeshkumarlogu145@gmail.com>
2023-12-18 22:15:51 +0530

    jwt error

--- Models/user.js
+++ Models/user.js
@@ -23,15 +23,15 @@ let userSchema = new mongoose.Schema({
 
 //Generate JWT token
 let generateJwtToken = (id) => {
-  return jwt.sign({ id }, process.env.SECRET_KEY);
+  return jwt.sign({ id }, process.env.SECRET_KEY, "Stack", {
+    expiresIn: "48h",
+  });
 };
 
 //Decode Jwt Token
 const decodeJwtToken = (token) => {
   try {
-    let decoded = jwt.verify(token, process.env.SECRET_KEY, "Stack", {
-      expiresIn: "48h",
-    });
+    let decoded = jwt.verify(token, process.env.SECRET_KEY);
     return decoded.id;
   } catch (error) {
     console.error("Error in Jwt Decoding", error);
